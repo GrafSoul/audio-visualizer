7e0b17ee10ea6615efd6647675ddb729e977425c
GrafSoul <grafsoul@gmail.com>
2020-11-18 17:58:35 +0500

    feat: Added the React Audio Spectrum functionality.

--- prototype/script.js
+++ prototype/script.js
@@ -1,55 +1,55 @@
 // Establish all variables that your Analyser will use
 let canvas,
-  ctx,
-  source,
-  context,
-  analyser,
-  fbc_array,
-  bars,
-  bar_x,
-  bar_width,
-  bar_height;
+    ctx,
+    source,
+    context,
+    analyser,
+    fbc_array,
+    bars,
+    bar_x,
+    bar_width,
+    bar_height;
 
 // Create a new instance of an audio object and adjust some of its properties
 let audio = new Audio();
-audio.src = "track.mp3";
+audio.src = 'track.mp3';
 audio.controls = true;
 audio.loop = true;
 audio.autoplay = false;
 
 // Initialize the MP3 player after the page loads all of its HTML into the window
-window.addEventListener("load", initMp3Player, false);
+window.addEventListener('load', initMp3Player, false);
 
 function initMp3Player() {
-  document.getElementById("audio_box").appendChild(audio);
-  canvas = document.getElementById("analyser");
+    document.getElementById('audio_box').appendChild(audio);
+    canvas = document.getElementById('analyser');
 
-  context = new AudioContext(); // AudioContext object instance
-  analyser = context.createAnalyser(); // AnalyserNode method
-  ctx = canvas.getContext("2d");
+    context = new AudioContext(); // AudioContext object instance
+    analyser = context.createAnalyser(); // AnalyserNode method
+    ctx = canvas.getContext('2d');
 
-  // Re-route audio playback into the processing graph of the AudioContext
-  source = context.createMediaElementSource(audio);
-  source.connect(analyser);
-  analyser.connect(context.destination);
+    // Re-route audio playback into the processing graph of the AudioContext
+    source = context.createMediaElementSource(audio);
+    source.connect(analyser);
+    analyser.connect(context.destination);
 
-  frameLooper();
+    frameLooper();
 }
 
 // frameLooper() animates any style of graphics you wish to the audio frequency
 // Looping at the default frame rate that the browser provides(approx. 60 FPS)
 function frameLooper() {
-  window.requestAnimationFrame(frameLooper);
-  fbc_array = new Uint8Array(analyser.frequencyBinCount);
-  analyser.getByteFrequencyData(fbc_array);
-  ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-  ctx.fillStyle = "#999999"; // Color of the bars
-  bars = 100;
-  for (var i = 0; i < bars; i++) {
-    bar_x = i * 3;
-    bar_width = 2;
-    bar_height = -(fbc_array[i] / 2);
-    //  fillRect( x, y, width, height ) // Explanation of the parameters below
-    ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
-  }
+    window.requestAnimationFrame(frameLooper);
+    fbc_array = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(fbc_array);
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+    ctx.fillStyle = '#999999'; // Color of the bars
+    bars = 100;
+    for (var i = 0; i < bars; i++) {
+        bar_x = i * 3;
+        bar_width = 2;
+        bar_height = -(fbc_array[i] / 2);
+        //  fillRect( x, y, width, height ) // Explanation of the parameters below
+        ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
+    }
 }
--- src/AudioVisualizer/AudioVisualizer.js
+++ src/AudioVisualizer/AudioVisualizer.js
@@ -1,8 +1,63 @@
-import React from 'react';
-import classes from './AudioVisualizer.module.scss';
+import React, { useRef } from 'react';
+import AudioSpectrum from 'react-audio-spectrum';
+
+import './AudioVisualizer.scss';
 
 const AudioVisualizer = () => {
-    return <div className={classes.component}>AudioVisualizer</div>;
+    const audioRef = useRef(document.querySelector('.audio'));
+
+    const handleFiles = (e) => {
+        let files = e.target.files;
+        audioRef.current.src = URL.createObjectURL(files[0]);
+        audioRef.current.load();
+    };
+
+    return (
+        <div className="container">
+            <h1>Audio Visualizer</h1>
+            <p>JavaScript, HTML and CSS.</p>
+            <p>
+                The application uses the{' '}
+                <a
+                    href="https://www.npmjs.com/package/react-audio-spectrum"
+                    target="_blank"
+                >
+                    React Audio Spectrum
+                </a>{' '}
+                module
+            </p>
+
+            <div className="player">
+                <div className="analyser-wrap">
+                    <AudioSpectrum
+                        id="audio-canvas"
+                        height={100}
+                        width={500}
+                        audioId={'audio-element'}
+                        capColor={'#007cc3'}
+                        capHeight={2}
+                        meterWidth={4}
+                        meterCount={512}
+                        meterColor={[
+                            { stop: 0, color: '#000' },
+                            { stop: 0.5, color: '#003452' },
+                            { stop: 1, color: '#003452' },
+                        ]}
+                        gap={2}
+                    />
+                </div>
+                <div className="audio-box">
+                    <audio
+                        ref={audioRef}
+                        controls
+                        className="audio"
+                        id="audio-element"
+                    ></audio>
+                    <input type="file" onChange={(e) => handleFiles(e)} />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default AudioVisualizer;
