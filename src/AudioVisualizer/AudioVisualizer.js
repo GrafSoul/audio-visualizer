1133d9899aebbc46bfdda9719dac265e446d9663
GrafSoul <grafsoul@gmail.com>
2020-11-18 13:33:31 +0500

    feat: Added AudioVisualizer component.

---
+++ src/AudioVisualizer/AudioVisualizer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './AudioVisualizer.module.scss';
+
+const AudioVisualizer = () => {
+    return <div className={classes.component}>AudioVisualizer</div>;
+};
+
+export default AudioVisualizer;
